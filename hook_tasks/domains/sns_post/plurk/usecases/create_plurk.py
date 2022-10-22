928cf380d36fb7dde99d02182933c844d56a3cea
Elton Chou <plscd748@gmail.com>
2022-10-23 02:04:49 +0800

    Refine plurk creation

--- hook_tasks/domains/sns_post/plurk/usecases/create_plurk.py
+++ hook_tasks/domains/sns_post/plurk/usecases/create_plurk.py
@@ -1,7 +1,13 @@
+from datetime import date
+from typing import Optional
+
 from babel.dates import format_date
 from hook_tasks.domains.sns_post.entities import ReleaseFeed
 from hook_tasks.domains.sns_post.plurk.entities import DOPlurkModel, PlurkConfig
+
 from .helpers import PlurkFormatHelper
+
+__all__ = ("create_plurk", "create_new_release_plurk")
 
 
 def create_plurk(content: str, plurk_config: PlurkConfig) -> DOPlurkModel:
@@ -11,24 +17,30 @@ def create_plurk(content: str, plurk_config: PlurkConfig) -> DOPlurkModel:
 def create_new_release_plurk(
     release_feed: ReleaseFeed, plurk_config: PlurkConfig
 ) -> DOPlurkModel:
-    release_date_text, price_text = "未定", "未定"
-    if release_feed.release_date:
-        release_date_text = format_date(
-            release_feed.release_date, "YYYY年 MMM", locale="zh"
-        )
+    category_text = _get_category_text(release_feed.rerelease)
+    post_header = _get_post_header_by_category(category_text)
+    post_body = _get_post_body_by_release_feed(release_feed=release_feed)
+    sep_line = _get_sep_line()
+    ad_block = _get_ad_block()
 
-    if release_feed.price:
-        price_text = f"{release_feed.price:,} 日圓"
+    content = post_header + post_body + sep_line + ad_block
 
-    category_text = "再販" if release_feed.rerelease else "新品"
+    return create_plurk(content=content, plurk_config=plurk_config)
 
-    post_category = PlurkFormatHelper.bold(f"[{category_text}速報]")
-    post_product_name = PlurkFormatHelper.link(release_feed.name, release_feed.url)
-    figure_hook_link = PlurkFormatHelper.link("Discord 速報訂閱", "https://bit.ly/3wj8Gpj")
 
-    content = (
-        ""
-        f"{post_category}\n"
+UNKNOWN_TEXT = "未定"
+
+
+def _get_post_header_by_category(category: str) -> str:
+    return PlurkFormatHelper.bold(f"[{category}速報]") + "\n"
+
+
+def _get_post_body_by_release_feed(release_feed: ReleaseFeed) -> str:
+    post_product_name = _get_linkable_product_name(release_feed.name, release_feed.url)
+    price_text = _get_price_text(release_feed.price)
+    release_date_text = _get_release_date_text(release_feed.release_date)
+
+    return (
         f"商品名: {post_product_name}\n"
         f"作品名稱: {release_feed.series}\n"
         f"製造商: {release_feed.maker}\n"
@@ -36,8 +48,33 @@ def create_new_release_plurk(
         f"發售日期: {release_date_text}\n"
         f"價格: {price_text}\n"
         f"{release_feed.media_image}\n"
-        "----------\n"
-        f"📨 {figure_hook_link}"
     )
 
-    return create_plurk(content=content, plurk_config=plurk_config)
+
+def _get_ad_block() -> str:
+    figure_hook_link = PlurkFormatHelper.link("Discord 速報訂閱", "https://bit.ly/3wj8Gpj")
+    return f"📨 {figure_hook_link}"
+
+
+def _get_linkable_product_name(product_name: str, url: str) -> str:
+    return PlurkFormatHelper.link(product_name, url)
+
+
+def _get_release_date_text(release_date: Optional[date]) -> str:
+    return (
+        format_date(release_date, "YYYY年 MMM", locale="zh")
+        if release_date
+        else UNKNOWN_TEXT
+    )
+
+
+def _get_price_text(price: Optional[int]) -> str:
+    return f"{price:,} 日圓" if price else UNKNOWN_TEXT
+
+
+def _get_category_text(is_rerelease: bool) -> str:
+    return "再販" if is_rerelease else "新品"
+
+
+def _get_sep_line() -> str:
+    return "----------\n"
