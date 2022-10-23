400ca2d2fb3ce2078ca9a48a97f3a726993f5ea9
Elton Chou <plscd748@gmail.com>
2022-10-23 18:24:53 +0800

    Improve discord entity RleaseEmbed teste coverage

--- tests/test_domains/test_sns_post/test_discord/test_discord_entities.py
+++ tests/test_domains/test_sns_post/test_discord/test_discord_entities.py
@@ -1,21 +1,36 @@
+from datetime import date
+
+import pytest
 from hook_tasks.domains.sns_post.discord.entities import ReleaseEmbed
 from hook_tasks.domains.sns_post.entities import ReleaseFeed
 
 
-def test_release_embed_entity(release_feed_factory):
-    release_feed: ReleaseFeed = release_feed_factory.build()
+class TestReleaseEmebed:
+    @pytest.fixture
+    def embed(
+        self,
+        release_feed_factory,
+    ):
+        release_feed: ReleaseFeed = release_feed_factory.build()
+        return ReleaseEmbed(title=release_feed.name, url=release_feed.url)
 
-    embed = (
-        ReleaseEmbed(
-            title=release_feed.name,
-            url=release_feed.url,
-        )
-        .set_nsfw(is_nsfw=release_feed.is_adult)
-        .set_price(price=release_feed.price)
-        .set_release_date(release_date=release_feed.release_date)
-        .set_size(size=release_feed.size)
-        .set_image(url=release_feed.media_image)
-        .set_scale(scale=release_feed.scale)
-    )
+    def test_set_nsfw(self, embed: ReleaseEmbed):
+        assert not embed.is_nsfw
+        embed.set_nsfw(is_nsfw=True)
+        assert embed.is_nsfw
 
-    assert embed.is_nsfw == release_feed.is_adult
+    def test_set_price(self, embed: ReleaseEmbed):
+        embed.set_price(price=None)
+        embed.set_price(price=12960)
+
+    def test_set_release_date(self, embed: ReleaseEmbed):
+        embed.set_release_date(release_date=None)
+        embed.set_release_date(release_date=date(2222, 2, 2))
+
+    def test_size(self, embed: ReleaseEmbed):
+        embed.set_size(size=None)
+        embed.set_size(size=250)
+
+    def test_set_scale(self, embed: ReleaseEmbed):
+        embed.set_scale(scale=None)
+        embed.set_scale(scale=7)
