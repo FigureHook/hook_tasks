84fd9bfe10c0799df965d910d0492ea54a2d1499
Elton Chou <plscd748@gmail.com>
2022-11-02 00:30:22 +0800

    Add webhook status updating

--- hook_tasks/infras/persistance/discord_webhook/discord_webhook_repository.py
+++ hook_tasks/infras/persistance/discord_webhook/discord_webhook_repository.py
@@ -1,19 +1,21 @@
-from hook_tasks.domains.sns_post.discord.entities.webhook_repository import (
-    DiscordWebhookRepositoryInterface,
+from typing import List
+
+from figure_hook_client.api.discord import (
+    get_webhooks_api_v1_webhooks_get,
+    patch_webhook_api_v1_webhooks_webhook_id_patch,
 )
+from figure_hook_client.client import AuthenticatedClient
+from figure_hook_client.models.decrypted_webhook_in_db import DecryptedWebhookInDB
+from figure_hook_client.models.webhook_lang import WebhookLang
+from figure_hook_client.models.webhook_update import WebhookUpdate
+from figure_hook_client.types import Unset
 from hook_tasks.domains.sns_post.discord.entities.webhook import (
     DiscordWebhook,
     DiscordWebhookLocale,
 )
-from figure_hook_client.client import AuthenticatedClient
-from typing import List
-from figure_hook_client.api.discord import (
-    get_webhooks_api_v1_webhooks_get,
-    update_webhook_api_v1_webhooks_channel_id_put,
+from hook_tasks.domains.sns_post.discord.entities.webhook_repository import (
+    DiscordWebhookRepositoryInterface,
 )
-from figure_hook_client.models.webhook_create import WebhookCreate
-from figure_hook_client.models.webhook_lang import WebhookLang
-from figure_hook_client.types import UNSET, Unset
 
 
 class DiscordWebhookRepository(DiscordWebhookRepositoryInterface):
@@ -23,29 +25,51 @@ class DiscordWebhookRepository(DiscordWebhookRepositoryInterface):
         self.client = client
 
     def get_all_langs(self) -> List[DiscordWebhookLocale]:
+        webhooks = get_webhooks_api_v1_webhooks_get.sync(client=self.client)
+        if webhooks:
+            return list(
+                set(
+                    DiscordWebhookLocaleMapper.from_webhook_lang(wb.lang)
+                    for wb in webhooks
+                )
+            )
         raise NotImplementedError
 
     def get_all_webhooks(self) -> List[DiscordWebhook]:
         webhooks = get_webhooks_api_v1_webhooks_get.sync(client=self.client)
         if webhooks:
             return [
-                DiscordWebhook(
-                    id=wb.id,
-                    token=wb.decrypted_token,
-                    lang=EmbedLocaleMapper.from_webhook_lang(wb.lang),
-                    locale=EmbedLocaleMapper.from_webhook_lang(wb.lang),
-                    channel_id=wb.channel_id,
-                    is_nsfw=bool(wb.is_nsfw),
-                    is_existed=bool(wb.is_existed),
-                )
-                for wb in webhooks
+                DiscordWebhookMapper.from_decrypted_webhook_in_db(wb) for wb in webhooks
             ]
         return []
 
-    def update_existed_status(
-        self, webhook: DiscordWebhook, is_existed: bool
+    def update_existed_status_by_webhook_id(
+        self, webhook_id: str, is_existed: bool
     ) -> DiscordWebhook:
+        webhook_patch = WebhookUpdate(is_existed=is_existed)
+        patched_webhook = patch_webhook_api_v1_webhooks_webhook_id_patch.sync(
+            webhook_id=webhook_id, client=self.client, json_body=webhook_patch
+        )
+        if isinstance(patched_webhook, DecryptedWebhookInDB):
+            return DiscordWebhookMapper.from_decrypted_webhook_in_db(patched_webhook)
+
         raise NotImplementedError
+
+
+class DiscordWebhookMapper:
+    @staticmethod
+    def from_decrypted_webhook_in_db(
+        db_webhook: DecryptedWebhookInDB,
+    ) -> DiscordWebhook:
+        return DiscordWebhook(
+            id=db_webhook.id,
+            token=db_webhook.decrypted_token,
+            lang=DiscordWebhookLocaleMapper.from_webhook_lang(db_webhook.lang),
+            locale=DiscordWebhookLocaleMapper.from_webhook_lang(db_webhook.lang),
+            channel_id=db_webhook.channel_id,
+            is_nsfw=bool(db_webhook.is_nsfw),
+            is_existed=bool(db_webhook.is_existed),
+        )
 
 
 _lang_mapping = {
@@ -55,7 +79,7 @@ _lang_mapping = {
 }
 
 
-class EmbedLocaleMapper:
+class DiscordWebhookLocaleMapper:
     @staticmethod
     def from_webhook_lang(webhook_lang: WebhookLang | Unset) -> DiscordWebhookLocale:
         if isinstance(webhook_lang, Unset):
