714a831483c8008fb135aaf556c0cf9ae8e9f6a0
Elton Chou <plscd748@gmail.com>
2022-11-05 13:06:29 +0800

    Fix typo

--- hook_tasks/domains/sns_post/discord/errors/release_embed_cache_errors.py
+++ hook_tasks/domains/sns_post/discord/errors/release_embed_cache_errors.py
@@ -3,5 +3,5 @@ from ..value_objects.release_embed_cache import ReleaseEmbedCacheKeyCriteria
 
 class SetEmbedCacheFailed(Exception):
     def __init__(self, key: ReleaseEmbedCacheKeyCriteria) -> None:
-        msg = f'Failed to set embed cache with key: "{key.to_key_str}".'
+        msg = f'Failed to set embed cache with key: "{key.to_key_str()}".'
         super().__init__(msg)
